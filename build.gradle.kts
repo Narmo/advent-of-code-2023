19296c9a3aa170bfc194ace015f216d636c0e515
Nikita Denin <nik@brite-apps.com>
2023-12-01 12:29:05 +0500

    finished day 01

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    sourceSets {
+        main {
+            java.srcDirs("src")
+        }
+    }
+
+    wrapper {
+        gradleVersion = "8.4"
+    }
+}
+
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions {
+	languageVersion = "1.9"
+}
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	val spelled = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int {
+		return input.sumOf {
+			val digits = it.filter { c -> c.isDigit() }
+			(digits.first().toString() + digits.last().toString()).toInt()
+		}
+	}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+	fun part2(input: List<String>): Int {
+		return input.sumOf { line ->
+			val digits = mutableListOf<Pair<Int, Int>>()
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+			line.forEachIndexed { index, c ->
+				if (c.isDigit()) {
+					digits.add(index to c.toString().toInt())
+				}
+			}
+
+			spelled.mapNotNull { mapping ->
+				var index = line.indexOf(mapping.key)
+
+				while (index != -1) {
+					digits.add(index to mapping.value)
+					index = line.indexOf(mapping.key, index + 1)
+				}
+			}
+
+			digits.sortBy { it.first }
+
+			(digits.first().second.toString() + digits.last().second.toString()).toInt()
+		}
+	}
+
+	val testInput1 = readInput("Day01_test1")
+	check(part1(testInput1) == 142)
+
+	val testInput2 = readInput("Day01_test2")
+	check(part2(testInput2) == 281)
+
+	val input = readInput("Day01")
+	println(part1(input))
+	println(part2(input))
 }
